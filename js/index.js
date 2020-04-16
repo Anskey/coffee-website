e59bf671c8bc2dca8a50e4105359ee229d9e5705
Anskey <anskey@gmail.com>
2020-04-16 20:23:40 -0230

    Coffee price added

--- js/index.js
+++ js/index.js
@@ -33,5 +33,38 @@ function validate(name,email,message){
         
     } else{
         alert.innerHTML = "";
+        /*Send email*/
+
+        emailUrl = "https://hooks.zapier.com/hooks/catch/5963107/o9gm325";
+        let emailObj = {
+            name: name,
+            email: email,
+            message: message,
+            to: "anskey@gmail.com"
+        };
+
+        let emailRequest = new XMLHttpRequest();
+        emailRequest.open("POST", emailUrl);
+        emailRequest.send(JSON.stringify(emailObj));
+        console.log()     
     }
 }
+
+/*Get coffee price*/
+
+coffeePriceUrl = "https://api.tradingeconomics.com/markets/search/coffee?c=guest:guest&f=json"
+let priceRequest = new XMLHttpRequest();
+priceRequest.addEventListener("load", processResponse);
+priceRequest.open("GET", coffeePriceUrl);
+priceRequest.send();
+function processResponse (e) {
+    e.preventDefault();
+    updatePrice(e);
+    console.log(JSON.parse(e.target.response));
+}
+
+function updatePrice(e){
+    document.getElementById("price").innerHTML = "$" + JSON.parse(e.target.response)[0].Last;
+}
+
+
